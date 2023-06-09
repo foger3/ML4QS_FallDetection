23b5f337f274a59b8db6478116b19826e3a9ea56
Nicole Tai <nicole1999224@gmail.com>
2023-06-10 00:59:43 +0200

    one hot encoding

--- src/data_clean_label.py
+++ src/data_clean_label.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from glob import glob
+from sklearn.preprocessing import OneHotEncoder
 
 def load_dataset_map(
     filepath: str,
@@ -49,7 +50,8 @@ def label_validation(df: pd.DataFrame, labels):
     print("{} labels{}\n".format(len(labels), ", not match."if len(labels) != dataset_row_num else ", match."))
 
 
-def labeling(df: pd.DataFrame, df_time: pd.DataFrame, labels: list):
+def labeling(df: pd.DataFrame, df_time: pd.DataFrame, labels: list, label_categories: list):
+    # labeling
     for i in range(0, df_time.shape[0], 2):
         start_time = df_time["experiment time"][i]
         end_time = df_time["experiment time"][i + 1]
@@ -57,12 +59,26 @@ def labeling(df: pd.DataFrame, df_time: pd.DataFrame, labels: list):
             (df["Time (s)"] >= start_time) & (df["Time (s)"] <= end_time)
         ].index
         df.loc[target_idx, ["Label"]] = labels[i // 2]
+    # one hot encode
+    df = one_hot_encoding(df, "Label", label_categories)
+
+    return df
 
 
+def one_hot_encoding(df, column_name, label_categories: list):
+    enc = OneHotEncoder(categories = [label_categories], sparse_output=False)
+    arr_label = enc.fit_transform(
+        df[column_name].to_numpy().reshape(-1, 1)
+    )
+    df_label = pd.DataFrame(arr_label, columns = enc.get_feature_names_out([column_name])).astype("int")
+    df_label.columns = label_categories
+    df.drop(columns=column_name, inplace=True)
+    df = pd.concat([df, df_label], axis = 1)
 
+    return df
 
 # labeling
-label_names = [
+label_categories = [
     "Label Walking",
     "Label Running",
     "Label Kneeing",
@@ -76,141 +92,141 @@ label_map = dict()
 
 # round1-luca
 labels = [
-    label_names[0],
-    label_names[1],
-    label_names[4],
-    label_names[5],
-    label_names[6],
-    label_names[3],
-    label_names[2],
-    label_names[2],
-    label_names[4],
-    label_names[1],
-    label_names[0],
-    label_names[6],
-    label_names[3],
-    label_names[6],
-    label_names[4],
-    label_names[6],
-    label_names[7],
-    label_names[2],
-    label_names[6],
-    label_names[7],
+    label_categories[0],
+    label_categories[1],
+    label_categories[4],
+    label_categories[5],
+    label_categories[6],
+    label_categories[3],
+    label_categories[2],
+    label_categories[2],
+    label_categories[4],
+    label_categories[1],
+    label_categories[0],
+    label_categories[6],
+    label_categories[3],
+    label_categories[6],
+    label_categories[4],
+    label_categories[6],
+    label_categories[7],
+    label_categories[2],
+    label_categories[6],
+    label_categories[7],
 ]
 label_map["round1-luca"] = labels
 # round1-nicole
 labels = [
-    label_names[0],
-    label_names[1],
-    label_names[4],
-    label_names[6],
-    label_names[2],
-    label_names[4],
-    label_names[6],
-    label_names[7],
-    label_names[7],
-    label_names[5],
-    label_names[4],
-    label_names[6]
+    label_categories[0],
+    label_categories[1],
+    label_categories[4],
+    label_categories[6],
+    label_categories[2],
+    label_categories[4],
+    label_categories[6],
+    label_categories[7],
+    label_categories[7],
+    label_categories[5],
+    label_categories[4],
+    label_categories[6]
 ]
 label_map["round1-nicole"] = labels
 # round1-sam
 labels = [
-    label_names[0],
-    label_names[6],
-    label_names[4],
-    label_names[6],
-    label_names[1],
-    label_names[2],
-    label_names[3],
-    label_names[6],
-    label_names[5],
-    label_names[6],
-    label_names[2],
-    label_names[0],
-    label_names[6],
-    label_names[4]
+    label_categories[0],
+    label_categories[6],
+    label_categories[4],
+    label_categories[6],
+    label_categories[1],
+    label_categories[2],
+    label_categories[3],
+    label_categories[6],
+    label_categories[5],
+    label_categories[6],
+    label_categories[2],
+    label_categories[0],
+    label_categories[6],
+    label_categories[4]
 ]
 label_map["round1-sam"] = labels
 # round2-luca
 labels = [
-    label_names[5],
-    label_names[0],
-    label_names[1],
-    label_names[6],
-    label_names[4],
-    label_names[3],
-    label_names[6],
-    label_names[2],
-    label_names[6],
-    label_names[5],
-    label_names[6],
-    label_names[4],
-    label_names[6],
-    label_names[2]
+    label_categories[5],
+    label_categories[0],
+    label_categories[1],
+    label_categories[6],
+    label_categories[4],
+    label_categories[3],
+    label_categories[6],
+    label_categories[2],
+    label_categories[6],
+    label_categories[5],
+    label_categories[6],
+    label_categories[4],
+    label_categories[6],
+    label_categories[2]
 ]
 label_map["round2-luca"] = labels
 # round2-nicole
 labels = [
-    label_names[4],
-    label_names[2],
-    label_names[3],
-    label_names[0],
-    label_names[1],
-    label_names[6],
-    label_names[4],
-    label_names[4],
-    label_names[6],
-    label_names[2],
-    label_names[0],
-    label_names[7],
-    label_names[3],
+    label_categories[4],
+    label_categories[2],
+    label_categories[3],
+    label_categories[0],
+    label_categories[1],
+    label_categories[6],
+    label_categories[4],
+    label_categories[4],
+    label_categories[6],
+    label_categories[2],
+    label_categories[0],
+    label_categories[7],
+    label_categories[3],
 ]
 label_map["round2-nicole"] = labels
 # round3-nicole
 labels = [
-    label_names[0],
-    label_names[4],
-    label_names[6],
-    label_names[6],
-    label_names[3],
-    label_names[6],
-    label_names[6],
-    label_names[2],
-    label_names[6],
-    label_names[1],
-    label_names[4],
-    label_names[7],
-    label_names[6],
-    label_names[0],
-    label_names[6],
-    label_names[6],
-    label_names[5],
-    label_names[3],
-    label_names[6],
-    label_names[2],
-    label_names[6],
+    label_categories[0],
+    label_categories[4],
+    label_categories[6],
+    label_categories[6],
+    label_categories[3],
+    label_categories[6],
+    label_categories[6],
+    label_categories[2],
+    label_categories[6],
+    label_categories[1],
+    label_categories[4],
+    label_categories[7],
+    label_categories[6],
+    label_categories[0],
+    label_categories[6],
+    label_categories[6],
+    label_categories[5],
+    label_categories[3],
+    label_categories[6],
+    label_categories[2],
+    label_categories[6],
 ]
 label_map["round3-nicole"] = labels
 # round3-sam
 labels = [
-    label_names[6],
-    label_names[2],
-    label_names[6],
-    label_names[0],
-    label_names[6],
-    label_names[7],
-    label_names[3],
-    label_names[1],
-    label_names[6],
-    label_names[4],
-    label_names[6],
-    label_names[2],
-    label_names[5],
-    label_names[5],
-    label_names[3],
-    label_names[6],
-    label_names[0],
+    label_categories[6],
+    label_categories[2],
+    label_categories[6],
+    label_categories[0],
+    label_categories[6],
+    label_categories[7],
+    label_categories[3],
+    label_categories[1],
+    label_categories[6],
+    label_categories[4],
+    label_categories[6],
+    label_categories[2],
+    label_categories[5],
+    label_categories[5],
+    label_categories[3],
+    label_categories[6],
+    label_categories[0],
 ]
 label_map["round3-sam"] = labels
 
@@ -231,6 +247,10 @@ for round in range(1, round_num + 1):
             df_result.fillna(method="ffill", inplace=True)
             df_result.dropna(how="any", inplace=True, ignore_index=True)
             # labeling
-            labeling(df_result, df_map["Time"], label_map[f"round{round}-{name}"])
+            df_result = labeling(df_result, df_map["Time"], label_map[f"round{round}-{name}"], label_categories)
+            # delete error labeling data
+            df_result.drop(df_result[df_result["Error"] == 1].index, inplace = True)
+            df_result.drop(columns="Error", inplace=True)
+
     df_cleaned = pd.concat([df_cleaned, df_result], ignore_index=True)
 df_cleaned.to_csv("../dataset/data_cleaned.csv", index = False)
