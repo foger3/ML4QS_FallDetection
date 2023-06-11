33084c5d952b11f2795f8b0a916bcd91d25a486a
Yung-sheng Tu <yssamtu@gmail.com>
2023-06-11 20:16:37 +0200

    add low pass filter

---
+++ src/LowPassFilter.py
@@ -0,0 +1,29 @@
+from scipy.signal import butter, lfilter, filtfilt
+import pandas as pd
+
+
+def low_pass_filter(
+    df: pd.DataFrame,
+    columns: list[str],
+    sampling_frequency: float,
+    cutoff_frequency: float = 1.5,
+    order: int = 10,
+    phase_shift: bool = True,
+):
+    # http://stackoverflow.com/questions/12093594/how-to-implement-band-pass-butterworth-filter-with-scipy-signal-butter
+    # Cutoff frequencies are expressed as the fraction of the Nyquist frequency, which is half the sampling frequency
+    nyq = 0.5 * sampling_frequency
+    for col in columns:
+        cut = cutoff_frequency / nyq
+        b, a = butter(order, cut, btype="low", output="ba", analog=False)
+        if phase_shift:
+            df[col + "_lowpass"] = filtfilt(b, a, df[col])
+        else:
+            df[col + "_lowpass"] = lfilter(b, a, df[col])
+    return df
+
+
+df = pd.read_csv("../dataset/data_cleaned.csv")
+granularity = 10
+columns = ["Magnetometer X (µT)"]
+df = low_pass_filter(df, columns, 1000 / granularity, 1.5)
