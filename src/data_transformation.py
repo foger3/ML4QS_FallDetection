240850b80496f42f855af15f98f65211b0959cec
Luca Thoms <luca.thoms@googlemail.com>
2023-06-15 16:22:45 +0200

    Refactored Classification Procedure + Streamlined Analysis

--- src/data_transformation.py
+++ src/data_transformation.py
@@ -3,42 +3,37 @@ from scipy.signal import butter, lfilter, filtfilt
 
 class DataTransformation:
 
-    def __init__(
-        self, 
+    # Interpolate the dataset based on previous/next values..
+    @staticmethod
+    def impute_interpolate(
         df: pd.DataFrame, 
         columns: list[str]
-    ):
-
-        self.df = df.copy()
-        self.columns = columns
-
-    # Interpolate the dataset based on previous/next values..
-    def impute_interpolate(
-        self
     ) -> pd.DataFrame:
         
-        self.df[self.columns] = self.df[self.columns].interpolate().ffill().bfill()
+        df[columns] = df[columns].interpolate().ffill().bfill()
 
-        return self.df
-
+        return df
+    
+    @staticmethod
     def low_pass_filter(
-        self,
+        df: pd.DataFrame, 
+        columns: list[str],
         sampling_frequency: float,
         cutoff_frequency: float = 1.5,
         order: int = 10,
         phase_shift: bool = True,
-    ):
+    ) -> pd.DataFrame:
         # http://stackoverflow.com/questions/12093594/how-to-implement-band-pass-butterworth-filter-with-scipy-signal-butter
         # Cutoff frequencies are expressed as the fraction of the Nyquist frequency, which is half the sampling frequency
         
         nyq = 0.5 * sampling_frequency
-        for col in self.columns:
+        for col in columns:
             cut = cutoff_frequency / nyq
             b, a = butter(order, cut, btype="low", output="ba", analog=False)
             if phase_shift:
-                self.df[col] = filtfilt(b, a, self.df[col])
+                df[col] = filtfilt(b, a, df[col])
             else:
-                self.df[col] = lfilter(b, a, self.df[col])
+                df[col] = lfilter(b, a, df[col])
                 
-        return self.df
+        return df
 
